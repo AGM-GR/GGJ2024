80b0ff7cdca512a2956f97c5906d073ea49b572b
Kiko <kikoalfaro2@gmail.com>
2024-01-27 13:41:50 +0100

    Update Jumper.cs

--- Assets/Jumper.cs
+++ Assets/Jumper.cs
@@ -47,17 +47,17 @@ public class Jumper : MonoBehaviour
     IEnumerator Jump()
     {
         IsJumping = true;
-        Animator.SetFloat("VerticalSpeed", 0);
+        Animator.SetTrigger("JumpRelease");
         yield return new WaitForSeconds(ReleaseTime);
         rb.velocity = Vector3.up * jumpForce;
-        Animator.SetFloat("VerticalSpeed", 0.5f);
+        Animator.SetTrigger("JumpAir");
         yield return null;
     }
 
 
     IEnumerator Land()
     {
-        Animator.SetFloat("VerticalSpeed", 0);
+        Animator.SetTrigger("JumpLanded");
         yield return new WaitForSeconds(LandingTime);
         IsJumping = false;
         yield return null;
@@ -65,7 +65,7 @@ public class Jumper : MonoBehaviour
 
     private void Fall()
     {
-        Animator.SetFloat("VerticalSpeed", 1f);
+        Animator.SetTrigger("JumpLanding");
         rb.velocity += Vector3.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
     }
 }
