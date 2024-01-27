620740f4f7e57bc5d4421d048fd799725e8530a6
Alejandro Guerrero Martínez <alejandroguerrero.gr@gmail.com>
2024-01-27 14:43:33 +0100

    Updated - Hitted with distance/curve settings and collisions aware

--- Assets/Hitter.cs
+++ Assets/Hitter.cs
@@ -7,11 +7,12 @@ using UnityEngine.InputSystem;
 public class Hitter : MonoBehaviour
 {
     public Collider HitTrigger;
-    public float ForcePower = 100f;
     public Animator Animator;
 
     public bool _isHitting;
 
+    [Header("Push Settings")]
+    public AnimationCurve PushCurve = AnimationCurve.Linear(0,0,1,1);
     public float PushDistance = 2f;
     public float PushVelocity = 10f;
 
@@ -39,27 +40,46 @@ public class Hitter : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (_isHitting) return;
+        if (other.attachedRigidbody == null) return;
 
         _isHitting = true;
         HitTrigger.enabled = false;
 
         Vector3 direction = transform.forward;
         //other.GetComponent<Rigidbody>().AddForce(direction * ForcePower, ForceMode.Impulse);
-        StartCoroutine(Push(other.transform, direction));        
+        StartCoroutine(Push(other.attachedRigidbody, direction));
     }
 
-    IEnumerator Push(Transform transform, Vector3 dir)
+    IEnumerator Push(Rigidbody pushedRigidbody, Vector3 dir)
     {
         float pushDuration = PushDistance / PushVelocity;
+        Vector3 initialPosition = pushedRigidbody.position;
+        Vector3 destPosition = pushedRigidbody.position + dir * PushDistance;
+
+        Vector3 nextPosition;
+        Vector3 lastPosition;
 
         float delta = 0;
-        while (delta <= pushDuration)
+        while (delta <= 1f)
         {
-            transform.position += dir * PushVelocity * Time.deltaTime;
-            delta += Time.deltaTime;
-            _isHitting = false;
-            yield return null;
+            lastPosition = pushedRigidbody.position;
+
+            delta += Time.fixedDeltaTime * (1f / pushDuration);
+
+            nextPosition = Vector3.Lerp(initialPosition, destPosition, PushCurve.Evaluate(delta));
+            pushedRigidbody.MovePosition(nextPosition);
+
+            yield return new WaitForFixedUpdate();
+
+            if (Vector3.Distance(pushedRigidbody.position, lastPosition) + 0.01f < Vector3.Distance(pushedRigidbody.position, nextPosition))
+            {
+                Debug.Log(name + " Hitted something!!");
+                break;
+            }
+
         }
+
+        _isHitting = false;
     }
 }
 
