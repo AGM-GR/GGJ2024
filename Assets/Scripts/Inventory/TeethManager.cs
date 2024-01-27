ee9ecfdde3541dabb45296824223e93055be9736
Kiko <kikoalfaro2@gmail.com>
2024-01-27 20:46:47 +0100

    ADD - New characters

--- Assets/Scripts/Inventory/TeethManager.cs
+++ Assets/Scripts/Inventory/TeethManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
@@ -7,14 +5,12 @@ public class TeethManager : MonoBehaviour
 {
     private Character _character;
     public Tooth toothPrefab;
+    public Transform SpawningPoint;
 
     public float ThrowForce = 12.0f;
     public float heightThrow = 10f;
     public float minThrow = 1f;
     public float maxThrow = 10f;
-
-    //List<TeethType> teethInventory = new List<TeethType>();
-
     public int TeethAmount = 3;
 
     private TeethWidget _widget;
@@ -27,28 +23,12 @@ public class TeethManager : MonoBehaviour
         UpdateWidget();
     }
 
-    //public TeethType GetRandomTeeth()
-    //{
-    //    return (TeethType)System.Enum.GetValues(typeof(TeethType)).GetValue(Random.Range(0, System.Enum.GetValues(typeof(TeethType)).Length - 1));
-    //}
-
     public void AddTooth(TeethType teeth)
     {
-        //teethInventory.Add(teeth);
         TeethAmount++;
         UpdateWidget();
     }
 
-    //public TeethType? RemoveTeeth()
-    //{
-    //    if (teethInventory.Count > 0)
-    //    {
-    //        TeethType toDrop = teethInventory[Random.Range(0, teethInventory.Count - 1)];
-    //        teethInventory.Remove(toDrop);
-    //        return toDrop;
-    //    }
-    //    return null;
-    //}
 
     [ContextMenu("Drop tooth")]
     public void DropTooth()
@@ -56,32 +36,18 @@ public class TeethManager : MonoBehaviour
         if (TeethAmount == 0) return;
 
         TeethAmount = Mathf.Max(TeethAmount - 1, 0);
-        Tooth newTooth = Instantiate(toothPrefab);
-        Vector3 nPos = transform.position;
-        nPos.y += heightThrow;
-        newTooth.transform.position = nPos;
+        Tooth newTooth = Instantiate(toothPrefab, SpawningPoint.position, Quaternion.identity);
 
         //Debug.Break();
         newTooth.SetAsPhysical();
-        ThrowObject(newTooth.Rigidbody);
+        //AddImpulseForce(newTooth.Rigidbody, ThrowForce);
+        newTooth.Rigidbody.AddForce((transform.up + transform.forward) * ThrowForce, ForceMode.Impulse);
+
         UpdateWidget();
     }
 
     private void UpdateWidget()
     {
         _widget.TeethAmountText.text = TeethAmount.ToString();
-    }
-
-    public void ThrowObject(Rigidbody rigidbody)
-    {
-
-        Vector3 direction = Vector3.up;
-        //direction = Quaternion.Euler(new Vector3(0, Random.Range(0, 359), 0)) * direction * Random.Range(minThrow, maxThrow);
-
-        // Reset forces
-        rigidbody.velocity = Vector3.zero;
-
-        // Add impulse
-        rigidbody.AddForce(direction.normalized * ThrowForce, ForceMode.Impulse); // To exclude the mass -> ForceMode.VelocityChange
     }
 }
