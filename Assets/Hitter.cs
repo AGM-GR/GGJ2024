67d74091e8987224643badfbf993c7a9cb97a432
Kiko <kikoalfaro2@gmail.com>
2024-01-27 13:13:30 +0100

    ADD - Hostia

--- Assets/Hitter.cs
+++ Assets/Hitter.cs
@@ -7,16 +7,19 @@ public class Hitter : MonoBehaviour
 {
     public Collider HitTrigger;
     public float ForcePower = 100f;
+    public Animator Animator;
+
+    public bool _isHitting;
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Z))
         {
-            TryHit();
+            Animator.SetTrigger("Hit");
         }
     }
 
-    private void TryHit()
+    public void TryHit()
     {
         StartCoroutine(HitEnabler());
     }
@@ -31,18 +34,25 @@ public class Hitter : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        if (_isHitting) return;
+
+        _isHitting = true;
+        HitTrigger.enabled = false;
+
         Vector3 direction = transform.forward;
         //other.GetComponent<Rigidbody>().AddForce(direction * ForcePower, ForceMode.Impulse);
-        StartCoroutine(Push(other.transform, direction));
+        StartCoroutine(Push(other.transform, direction));        
     }
 
     IEnumerator Push(Transform transform, Vector3 dir)
     {
+        Debug.Log("Push");
         float delta = 0;
         while (delta <= 0.5f)
         {
             transform.position += dir * ForcePower * Time.deltaTime;
             delta += Time.deltaTime;
+            _isHitting = false;
             yield return null;
         }
     }
