6e62d50b61c0cf28515d6bd6af26270502bffabb
Benji Rus <8707038+benrusza@users.noreply.github.com>
2024-01-28 17:24:42 +0100

    door

--- Assets/FinalDoor.cs
+++ Assets/FinalDoor.cs
@@ -8,6 +8,8 @@ public class FinalDoor : MonoBehaviour
     public Collider Trigger;
     public GameObject PortalFX;
 
+    public GameObject FinalSet;
+
     private void Start()
     {
         
@@ -34,9 +36,47 @@ public class FinalDoor : MonoBehaviour
             Character character = other.GetComponent<Character>();
 
             if(character.GetComponent<TeethManager>().GoldTeeth>0){
-                Debug.Log(character.Name + "WINS");
+                FinalSet.SetActive(true);
+
+                Debug.Log(character.Name + " WINS");
                 PlayerPrefs.SetInt("Winner", character.CharacterIndex);
                 //SceneManager
+              
+                if(character.CharacterIndex == 2){
+                    GameObject.Find("Richarl_RIG_end").SetActive(true);
+                    GameObject.Find("Richarl_RIG_end_1").SetActive(false);
+
+                    GameObject.Find("LaJenny_RIG_end").SetActive(false);
+                    GameObject.Find("ElRulas_RIG_end").SetActive(false);
+                    GameObject.Find("LaVane_RIG_end").SetActive(false);
+                }
+                else if(character.CharacterIndex == 1){
+                    GameObject.Find("LaJenny_RIG_end").SetActive(true);
+                    GameObject.Find("LaJenny_RIG_end_1").SetActive(false);
+
+                    GameObject.Find("Richarl_RIG_end").SetActive(false);
+                    GameObject.Find("ElRulas_RIG_end").SetActive(false);
+                    GameObject.Find("LaVane_RIG_end").SetActive(false);
+                }
+                else if(character.CharacterIndex == 0){
+                    GameObject.Find("ElRulas_RIG_end").SetActive(true);
+                    GameObject.Find("ElRulas_RIG_end_1").SetActive(false);
+
+                    GameObject.Find("Richarl_RIG_end").SetActive(false);
+                    GameObject.Find("LaJenny_RIG_end").SetActive(false);
+                    GameObject.Find("LaVane_RIG_end").SetActive(false);
+                }
+                else if(character.CharacterIndex == 3){
+                    GameObject.Find("LaVane_RIG_end").SetActive(true);
+                    GameObject.Find("LaVane_RIG_end_1").SetActive(false);
+
+                    GameObject.Find("Richarl_RIG_end").SetActive(false);
+                    GameObject.Find("LaJenny_RIG_end").SetActive(false);
+                    GameObject.Find("ElRulas_RIG_end").SetActive(false);
+                }
+
+                
+
             }
            
         }
